0434b4a60993376c60e8b88c51707e163c3f1d59
Halim Ghazi <39797181+r00texploit@users.noreply.github.com>
2023-11-26 11:03:59 +0200

    adding getx funcationality

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,7 @@ import 'package:flutter_calculator/bindings/my_bindings.dart';
 import 'package:flutter_calculator/screen/main_screen.dart';
 import 'package:get/get.dart';
 
-//
-// Created by CodeWithFlexZ
-// Tutorials on my YouTube
-//
-//! Instagram
-//! @CodeWithFlexZ
-//
-//? GitHub
-//? AmirBayat0
-//
-//* YouTube
-//* Programming with FlexZ
-//
+
 
 void main() {
   runApp(const MyApp());

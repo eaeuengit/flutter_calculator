2850e1e73f238e6003ad0775fc880f1dfa4d2463
Halim Ghazi <39797181+r00texploit@users.noreply.github.com>
2023-11-26 10:54:23 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calculator/bindings/my_bindings.dart';
+import 'package:flutter_calculator/screen/main_screen.dart';
+import 'package:get/get.dart';
+
+//
+// Created by CodeWithFlexZ
+// Tutorials on my YouTube
+//
+//! Instagram
+//! @CodeWithFlexZ
+//
+//? GitHub
+//? AmirBayat0
+//
+//* YouTube
+//* Programming with FlexZ
+//
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialBinding: MyBindings(),
+      title: "Flutter Calculator",
+      home: MainScreen(),
+    );
+  }
+}
